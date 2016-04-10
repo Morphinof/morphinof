23d0ed41e45f5f3f94a852166b45a1a7d219b8a8
Morphinof <morphinof@gmail.com>
2016-04-10 16:05:47 +0200

    Inspinia bundle - Working until Metrics section

--- src/InspiniaBundle/Controller/DefaultController.php
+++ src/InspiniaBundle/Controller/DefaultController.php
@@ -40,4 +40,79 @@ class DefaultController extends Controller
     {
         return $this->render('InspiniaBundle:Default:layouts.html.twig');
     }
+
+    public function graphFlotAction()
+    {
+        return $this->render('InspiniaBundle:Default:graph-flot.html.twig');
+    }
+
+    public function graphMorrisAction()
+    {
+        return $this->render('InspiniaBundle:Default:graph-morris.html.twig');
+    }
+
+    public function graphRickshawAction()
+    {
+        return $this->render('InspiniaBundle:Default:graph-rickshaw.html.twig');
+    }
+
+    public function graphChartJsAction()
+    {
+        return $this->render('InspiniaBundle:Default:graph-chartjs.html.twig');
+    }
+
+    public function graphChartistAction()
+    {
+        return $this->render('InspiniaBundle:Default:graph-chartist.html.twig');
+    }
+
+    public function graphC3Action()
+    {
+        return $this->render('InspiniaBundle:Default:graph-c3.html.twig');
+    }
+
+    public function graphPeityAction()
+    {
+        return $this->render('InspiniaBundle:Default:graph-peity.html.twig');
+    }
+
+    public function graphSparklineAction()
+    {
+        return $this->render('InspiniaBundle:Default:graph-sparkline.html.twig');
+    }
+
+    public function mailboxAction()
+    {
+        return $this->render('InspiniaBundle:Default:mailbox.html.twig');
+    }
+
+    public function mailDetailAction()
+    {
+        return $this->render('InspiniaBundle:Default:mail-detail.html.twig');
+    }
+
+    public function mailComposeAction()
+    {
+        return $this->render('InspiniaBundle:Default:mail-compose.html.twig');
+    }
+
+    public function emailTemplatesAction()
+    {
+        return $this->render('InspiniaBundle:Default:email-templates.html.twig');
+    }
+
+    public function emailActionAction()
+    {
+        return $this->render('InspiniaBundle:Default:email-templates/action.html.twig');
+    }
+
+    public function emailAlertAction()
+    {
+        return $this->render('InspiniaBundle:Default:email-templates/alert.html.twig');
+    }
+
+    public function emailBillingAction()
+    {
+        return $this->render('InspiniaBundle:Default:email-templates/billing.html.twig');
+    }
 }
